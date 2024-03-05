4ee9f23d75ae0565088b33d1c83644301e38701e
deepjoy9 <deepjoysarkar9@gmail.com>
2024-03-05 21:45:09 +0530

    Building Homepage with React

---
+++ client/src/App.jsx
@@ -0,0 +1,70 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Blog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1709589865176-7c6ede164354?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8" />
+        </div>
+        <div className="texts">
+          <h2>A view of postntain range with a lake in the foreground</h2>
+          <p className="info">
+            <a className="author">Deepjoy Sarkar</a>
+            <time>2024-05-03 21:32</time>
+          </p>
+          <p className="summary">
+            Download thpostee HD photo of lofoten, norway, road, and mountains
+            in Norway by Vidar Nordli-Mathisen (@vidarnm)
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1709589865176-7c6ede164354?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8" />
+        </div>
+        <div className="texts">
+          <h2>A view of postntain range with a lake in the foreground</h2>
+          <p className="info">
+            <a className="author">Deepjoy Sarkar</a>
+            <time>2024-05-03 21:32</time>
+          </p>
+          <p className="summary">
+            Download thpostee HD photo of lofoten, norway, road, and mountains
+            in Norway by Vidar Nordli-Mathisen (@vidarnm)
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1709589865176-7c6ede164354?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8" />
+        </div>
+        <div className="texts">
+          <h2>A view of postntain range with a lake in the foreground</h2>
+          <p className="info">
+            <a className="author">Deepjoy Sarkar</a>
+            <time>2024-05-03 21:32</time>
+          </p>
+          <p className="summary">
+            Download thpostee HD photo of lofoten, norway, road, and mountains
+            in Norway by Vidar Nordli-Mathisen (@vidarnm)
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
